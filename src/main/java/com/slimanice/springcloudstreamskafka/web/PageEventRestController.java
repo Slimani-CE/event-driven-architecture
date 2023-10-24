93da4fd16e0fdc19c89c3e679d8b025add6fa511
Mustapha SLIMANI <itsmustaphahere@gmail.com>
2023-10-24 21:57:49 +0100

    update

--- src/main/java/com/slimanice/springcloudstreamskafka/web/PageEventRestController.java
+++ src/main/java/com/slimanice/springcloudstreamskafka/web/PageEventRestController.java
@@ -3,10 +3,14 @@ package com.slimanice.springcloudstreamskafka.web;
 import com.slimanice.springcloudstreamskafka.entities.PageEvent;
 import lombok.AllArgsConstructor;
 import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
+import java.awt.*;
+import java.time.Duration;
 import java.util.Date;
 import java.util.Random;
 
@@ -20,4 +24,12 @@ public class PageEventRestController {
         streamBridge.send(topic, pageEvent);
         return pageEvent;
     }
+
+    @GetMapping(path = "/analytics", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> analytics() {
+        return Flux.interval(Duration.ofSeconds(1)).map(
+
+        );
+    }
+
 }
